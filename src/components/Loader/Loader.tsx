fb065704f3f68acd901b42519d3c3c1d5433e1d4
vika59216 <viktoriiadupreez@gmail.com>
2024-06-28 17:40:23 +0200

    hw2

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,24 +1,26 @@
-import React from 'react'
-import { RotatingLines } from "react-loader-spinner"
+import React, { FC } from "react";
+import { RotatingLines } from "react-loader-spinner";
+const Loader: FC = () => {
 
+  const props={
+     visible:true,
+    height: "96",
+  width:"96",
+  color:"grey",
+  strokeWidth:"5",
+  animationDuration:"0.75",
+    ariaLabel: "rotating-lines-loading",
+  
+ 
 
+  }
 
-const Loader = () => {
   return (
-      <div>
-          <RotatingLines
-  visible={true}
-  height="96"
-  width="96"
-  color="grey"
-  strokeWidth="5"
-  animationDuration="0.75"
-  ariaLabel="rotating-lines-loading"
-  wrapperStyle={{}}
-  wrapperClass=""
-  />
+    <div>
+      <RotatingLines
+      {...props}
+      />
     </div>
-  )
-}
-
-export default Loader
+  );
+};
+export default Loader;
